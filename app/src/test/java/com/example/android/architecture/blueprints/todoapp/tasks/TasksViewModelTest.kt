787c8dd23b35eab4dec0806d2b34c2edafbc7cb0
sabiyyah <65693581+sabiyyah@users.noreply.github.com>
2020-05-28 23:53:58 -0400

    gradle dependencies

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+
+        // Given a fresh TasksViewModel
+
+    }
+}
