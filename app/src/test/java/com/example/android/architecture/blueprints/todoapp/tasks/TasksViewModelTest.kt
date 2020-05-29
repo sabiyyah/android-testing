7d499c2361cfe82372d881a5d39b5b2a6aed7b43
sabiyyah <65693581+sabiyyah@users.noreply.github.com>
2020-05-29 00:26:00 -0400

    Assertions for LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -17,37 +17,21 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
 
         // Given a fresh TasksViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-        // Create observer
-        val observer = Observer<Event<Unit>> {}
-        try {
-            // Observe LiveData forever
-            tasksViewModel.newTaskEvent.observeForever(observer)
+        // When adding a new task
+        tasksViewModel.addNewTask()
 
-            // When adding a new task
-            tasksViewModel.addNewTask()
+        // Then new task event triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue ()
 
-            // Then new task event triggered
-            val value = tasksViewModel.newTaskEvent.value
-            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
-        } finally {
-            // Remove observer no matter what
-            tasksViewModel.newTaskEvent.removeObserver(observer)
-        }
-
-
-
-
-        // TODO test LiveData
-    }
-
-    class TasksViewModelTest {
-        @get:Rule
-        var instantExecutorRule = InstantTaskExecutorRule()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
     }
 }
