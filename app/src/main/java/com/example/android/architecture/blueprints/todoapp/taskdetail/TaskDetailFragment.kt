1c99d449eef53c79e5ed58277210081cdcdc5656
sabiyyah <65693581+sabiyyah@users.noreply.github.com>
2020-05-29 23:35:43 -0400

    Using ServiceLocator in Application

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.kt
@@ -28,6 +28,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.architecture.blueprints.todoapp.EventObserver
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.TodoApplication
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.databinding.TaskdetailFragBinding
 import com.example.android.architecture.blueprints.todoapp.tasks.DELETE_RESULT_OK
@@ -43,7 +44,7 @@ class TaskDetailFragment : Fragment() {
 
     private val args: TaskDetailFragmentArgs by navArgs()
 
-    private val viewModel by viewModels<TaskDetailViewModel> { TaskDetailViewModelFactory(DefaultTasksRepository.getRepository(requireActivity().application)) }
+    private val viewModel by viewModels<TaskDetailViewModel> { TaskDetailViewModelFactory((requireContext().applicationContext as TodoApplication).taskRepository)}
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
